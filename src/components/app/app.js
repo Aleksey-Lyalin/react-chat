d233f817055b8f754437c87292af92a92257b325
Aleksey Lyalin <alvas.lyalin@yandex.ru>
2019-09-16 07:11:11 +0300

    Nav Bar

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -3,10 +3,15 @@ import React from 'react';
 // import SearchPanel from '../search-panel/search-panel'
 // import PostStatusFilter from '../post-status-filter/post-status-filter'
 import LoginForm from '../login-form/login-form'
+import NavBar from '../nav-bar/nav-bar'
 
 const App = () => {
     return (
+    <div>
+        <NavBar/>
         <LoginForm/>
+        
+        </div>
     )
 }
 export default App;
